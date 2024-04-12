f721edc474e1146f509d92be0a89771e15655c85
Sky Fluff <millerjp224@gmail.com>
2024-04-12 00:19:15 -0500

    Got simple card animation working

--- src/Elements/AppContainer/AppContainer.tsx
+++ src/Elements/AppContainer/AppContainer.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import Canvas from "../Canvas/Canvas";
 import Toolbar from "../Toolbar/Toolbar";
@@ -6,12 +6,21 @@ import Background from "../Background/Background";
 import Card from "../Card/Card";
 
 const AppContainer =() => {
+    const [position, setPosition] = useState<{x: number, y: number}>({x: 100, y: 100});
+
     const canvasRef = useRef<HTMLCanvasElement>();
+
+    useEffect(() => {
+        setInterval(() => {
+            setPosition(({x,y}) => ({x:x+10,y:y+10}));
+        }, 1000);
+    }, []);
     return(
         <UserContext.Provider value={{canvasRef}}>
             <Canvas ref={(ref)=>{canvasRef.current=ref||undefined}} />
             <Background src='url(/vaporwave.jpg)' />
-            <Card width={500} position={{x: 100, y: 100}}/>
+            <Card width={100} position={position} />
+            <Card width={100} position={{x: 300, y: 200}} />
             <Toolbar />
         </UserContext.Provider>
     )
--- src/Elements/Card/Card.tsx
+++ src/Elements/Card/Card.tsx
@@ -7,16 +7,17 @@ interface CardProps {
 }
 
 const CardFrame = styled.div<{width: number, x: number, y: number}>`
-    width: ${({width})=>width};
-    height: ${({width})=>width*1.5454}; //standard playing card dimensions
+    width: ${({width})=>width}px;
+    height: ${({width})=>width*1.5454}px; //standard playing card dimensions
     position: fixed;
-    top: ${({y,width})=>y - (width*1.5454/2)};
-    left: ${({x, width})=>x - (width/2)};
+    top: ${({y,width})=>y - (width*1.5454/2)}px;
+    left: ${({x, width})=>x - (width/2)}px;
     pointer-events: none;
     border: 1px solid black;
     border-radius: ${({width})=>width*0.0551181102}px;
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
     background: linear-gradient(to bottom right, white, lightgrey);
+    transition: top linear 1s, left linear 1s;
 `;
 
 const Card = ({width, position}: CardProps) => {
